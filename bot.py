28c808d1911abb7c862261325935c9e14e15c3d7
Mikedreyeu <Mikedreyeu@gmail.com>
2018-07-18 01:31:19 +0300

    Add send_audio method

--- bot.py
+++ bot.py
@@ -1,15 +1,16 @@
 import requests
 import json
-import vk
+from vk import *
+import io
 
 BOT_TOKEN = '556191721:AAH11vENmvGlnHlDKnGiwWCnIIIdW5v-ntA'
 
 BASE_URL = 'https://api.telegram.org/bot' + BOT_TOKEN + '/'
 
 
-def log_json_update(update):
-    with open('updates.log', 'a') as log_file:
-        json.dump(update, log_file, indent=2, ensure_ascii=False)
+def log_json(data, filename):
+    with open(filename, 'a') as log_file:
+        json.dump(data, log_file, indent=2, ensure_ascii=False)
         log_file.write('\n\n')
 
 def handleUpdate(update):
@@ -18,13 +19,22 @@ def handleUpdate(update):
         message_text = update['message']['text']
     except KeyError:
         return 'KeyError: \'text\''
+    returned_messages = []
     if message_text == '/start':
-        send_message(chat_id, u'🔪')
+        rm = send_message(chat_id, u'🔪')
     elif message_text == '/help':
-        send_message(chat_id, 'no')
+        rm = send_message(chat_id, 'no')
+    elif message_text == 'pt':
+        rm = send_photo(chat_id, 'https://pp.userapi.com//c626516//v626516637//57203//pCdItq4XreQ.jpg')
+        returned_messages.append(rm)
+        rm = send_message(chat_id, 'Мик Гордон подтвердил, что саундтрек DOOM Eternal будет иметь тот же стиль, что у перезапуска.\n\n\"Мы потратили очень много времени, чтобы определить звук DOOM, и мы не собираемся отказываться от него\"\n\nГотовьте свои ушки!', 'https://vk.com/dev/objects/post', 'VK link')
+    elif message_text == 'ptg':
+        rm = send_photo(chat_id, 'https://pp.userapi.com//c626516//v626516637//57203//pCdItq4XreQ.jpg', 'https://vk.com/dev/objects/post', 'VK link', 'Мик Гордон подтвердил, что саундтрек DOOM Eternal будет иметь тот же стиль, что у перезапуска.')
+    elif message_text == 'sa':
+        rm = send_audio(chat_id, 'https://cs9-17v4.vkuseraudio.net/p5/6914a6d0d82c38.mp3')
+    returned_messages.append(rm)
+    log_json(returned_messages, 'post_replies.log')
 
-#TODO
-# Create one handler to avoid duplication
 
 def send_message(chat_id, text, inline_url=None, inline_text=None):
     url = BASE_URL + 'sendMessage'
@@ -38,7 +48,7 @@ def send_message(chat_id, text, inline_url=None, inline_text=None):
     return post.json()
 
 
-def send_photo(chat_id, photo, caption='', inline_url=None, inline_text=None):
+def send_photo(chat_id, photo, inline_url=None, inline_text=None, caption=''):
     url = BASE_URL + 'sendPhoto'
     if inline_url != None and inline_text != None:
         ikb = {'text': inline_text, 'url': inline_url}
@@ -47,4 +57,13 @@ def send_photo(chat_id, photo, caption='', inline_url=None, inline_text=None):
     else:
         message = {'chat_id': chat_id, 'photo': photo, 'caption': caption}
     post = requests.post(url, json=message)
+    return post.json()
+
+
+def send_audio(chat_id, audio_url, performer='Unknown', title='Unknown'):
+    url = '{}sendAudio?chat_id={}&performer={}&title={}'.format(BASE_URL, chat_id, performer, title)
+    remote_file = requests.get(audio_url)
+    file1 = io.BytesIO(remote_file.content)
+    files = dict({'audio': file1})
+    post = requests.post(url, files=files)
     return post.json()
--- vk.py
+++ vk.py
@@ -5,11 +5,13 @@ import re
 VK_ACCESS_TOKEN = 'd6b48fd0deea32e34f31285946d60dbb8269b60258d023b572290d9f33c3ea419476668ef5277a27c0ef9'
 VK_BASE_URL = 'https://api.vk.com/method/'
 
+
 def get_posts(domain, count):
     url = VK_BASE_URL + 'wall.get?domain=' + domain 
     url += '&count=' + count + '&filter=owner&v=5.80&access_token=' + VK_ACCESS_TOKEN
     posts = requests.get(url)
     return posts.json()
+
 
 def parse_posts(posts):
     parsed_posts = []
@@ -18,17 +20,22 @@ def parse_posts(posts):
             continue
         post_id = item['id']
         text = item['text']
+        photos_url = []
+        videos_url = []
+        pinned_audio = None
         for attachment in item['attachments']:
             if(attachment['type'] == 'photo'):
                 for size in reversed(attachment['photo']['sizes']):
                     if(size['type'] == 'z' or size['type'] == 'y' 
                             or size['type'] == 'x' or size['type'] == 'm' or size['type'] == 's'):
-                        photo_url = size['src']
+                        photos_url.append(size['src'])
                         break
-            if(attachment['type'] == 'video'):
-                video = parse_video(attachment['video'])
-
-            
+            elif(attachment['type'] == 'video'):
+                videos_url.append(parse_video(attachment['video']))
+            elif(attachment['type'] == 'audio'):
+                pinned_audio = attachment['audio']['artist'] + ' - ' + attachment['audio']['title']
+            elif(attachment['type'] == 'link'):
+                pass
                         
 def parse_video(video):
     '''Return video url from VK video object.
@@ -36,7 +43,6 @@ def parse_video(video):
     Args:
     video -- VK video object
     '''
-
     url = VK_BASE_URL + 'video.get?owner_id=' + video['owner_id'] 
     url += '&videos=' + video['owner_id'] + '_' + video['id'] 
     url += 'count=1&extended=0&v=5.80&access_token=' + VK_ACCESS_TOKEN
